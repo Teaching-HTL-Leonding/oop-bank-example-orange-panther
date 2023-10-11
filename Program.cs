d4a56b45e48d90be8bedc03b3ba8a1cbad0a7691
Kathi Einzenberger <113719929+orange-panther@users.noreply.github.com>
2023-10-11 11:59:23 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,30 @@
 ﻿using Banking.Logic;
 
-Console.Write("Type of account ([c]hecking, [b]usiness, [s]avings: )");
+DateOnly openDate = DateOnly.MinValue, fixedDate = DateOnly.MinValue;
+Console.Write("Type of account ([c]hecking, [b]usiness, [s]avings, [f]ixed deposite: ");
 var type = Console.ReadLine()!;
 Account myAccount = type switch
 {
     "c" => new CheckingAccount(),
     "b" => new BusinessAccount(),
     "s" => new SavingsAccount(),
+    "f" => new FixedDeposite(openDate, fixedDate),
     _ => throw new ArgumentException("Invalid account type.")
 };
 Console.Write("Account number: ");
-myAccount.AccountNumber= Console.ReadLine()!;
+myAccount.AccountNumber = Console.ReadLine()!;
 Console.Write("Account holder: ");
 myAccount.AccountHolder = Console.ReadLine()!;
 Console.Write("Current balance: ");
+
 myAccount.CurrentBalance = decimal.Parse(Console.ReadLine()!);
+if (myAccount is FixedDeposite fixedDeposite)  
+{
+    Console.Write("Opening Date [yyyy-mm-dd]: ");
+    fixedDeposite.OpeningDate = DateOnly.Parse(Console.ReadLine()!);
+    Console.Write("Fixed Until Date [yyyy-mm-dd]: ");
+    fixedDeposite.FixedUntil = DateOnly.Parse(Console.ReadLine()!);
+}
 
 var myTransaction = new Transaction();
 
